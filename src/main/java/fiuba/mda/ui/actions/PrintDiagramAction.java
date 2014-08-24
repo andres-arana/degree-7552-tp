68f9cfcad301e1ad7743138588908e0b395aab48
scampa <sabrina_campa@yahoo.com.ar>
2014-08-24 13:38:17 -0300

    Implementacion de metodo run() de PrintDiagramAction

--- src/main/java/fiuba/mda/ui/actions/PrintDiagramAction.java
+++ src/main/java/fiuba/mda/ui/actions/PrintDiagramAction.java
@@ -1,6 +1,18 @@
 package fiuba.mda.ui.actions;
 
 import org.eclipse.jface.action.Action;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.printing.PrintDialog;
+import org.eclipse.swt.printing.Printer;
+import org.eclipse.swt.printing.PrinterData;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 
 import com.google.inject.Inject;
@@ -44,7 +56,60 @@ public class PrintDiagramAction extends Action {
 
 	@Override
 	public void run() {
-		ExportToImageAction.exportImage(5, "diagrama.png", this.boundDiagram.getDiagramFigure());
-    }
+	    try {
+	      // Prompt the user for an image file
+	      FileDialog fileChooser = new FileDialog(shell, SWT.OPEN);
+	      String fileName = fileChooser.open();
 
+	      if (fileName == null) { return; }
+
+	      // Load the image
+	      org.eclipse.swt.graphics.ImageLoader loader = new org.eclipse.swt.graphics.ImageLoader();
+	      ImageData[] imageData = loader.load(fileName);
+
+	      if (imageData.length > 0) {
+	        // Show the Choose Printer dialog
+	        PrintDialog dialog = new PrintDialog(shell, SWT.NULL);
+	        PrinterData printerData = dialog.open();
+
+	        if (printerData != null) {
+	          // Create the printer object
+	          Printer printer = new Printer(printerData);
+	  
+	          // Resolution
+	          int scaleFactor = 3;
+	  
+	          // Determine the bounds of the entire area of the printer
+	          Rectangle trim = printer.computeTrim(0, 0, 0, 0);
+
+	          // Start the print job
+	          if (printer.startJob(fileName)) {
+	            if (printer.startPage()) {
+	              GC gc = new GC(printer);
+	              Image printerImage = new Image(printer, imageData[0]);
+	              
+	              // Draw the image
+	              gc.drawImage(printerImage, 0, 0, imageData[0].width,
+	                imageData[0].height, -trim.x, -trim.y, 
+	                scaleFactor * imageData[0].width, 
+	                scaleFactor * imageData[0].height);
+	  
+	              // Clean up
+	              printerImage.dispose();
+	              gc.dispose();
+	              printer.endPage();
+	            }
+	          }
+	          // End the job and dispose the printer
+	          printer.endJob();
+	          printer.dispose();
+	        }
+	      }
+	    } catch (Exception e) {
+	      MessageBox messageBox = new MessageBox(shell, SWT.ICON_ERROR);
+	      messageBox.setMessage("Error al imprimir imagen");
+	      messageBox.open();
+	    }
+	  }
+	
 }
